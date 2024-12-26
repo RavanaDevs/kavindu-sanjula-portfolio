af46820b37145b97ba7ac797a6f473809010178d
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-12-26 09:59:22 +0530

    Sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { HeroSection } from '@/components/sections/hero';
 import { Navigation } from '@/components/navigation';
-import { AboutSection } from '@/components/sections/about';
+import { AboutSection } from '@/components/sections/about/index';
 import { SkillsSection } from '@/components/sections/skills/index';
 import { ProjectsSection } from '@/components/sections/projects/index';
 import { ContactSection } from '@/components/sections/contact';
