5a8363f43af0c9aa628290ad92433c8c19d87b9c
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2025-01-14 22:50:09 +0530

    Download resume

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+
+export function downloadResume() {
+  window.open("/docs/kavindu-sanjula-resume.pdf");
+};
