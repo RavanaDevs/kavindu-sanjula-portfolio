c5eef835894868da2dad6045eb22dfe27740e2d4
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2025-01-27 10:08:41 +0530

    Image Path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: false,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "*",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
